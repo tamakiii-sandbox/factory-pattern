7f6378428405c1de1db0e3b9693d5ab0678cb847
tamakiii <tamaki.htm@gmail.com>
2014-09-19 22:15:51 +0900

    add Concretes\RegisterableFactoryInjectable

---
+++ src/Concretes/RegisterableFactoryInjectable.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Concretehouse\Dp\Factory\Concretes;
+
+use Concretehouse\Dp\Factory\Registerable;
+use Concretehouse\Dp\Factory\FunctionsInterface;
+use Concretehouse\Dp\Factory\MatchableInterface;
+use Concretehouse\Dp\Factory\MatcherInjectableInterface;
+
+/**
+ * Matcher-injectable registerable factory class.
+ */
+class RegisterableFactoryInjectable
+    extends Registerable
+    implements MatcherInjectableInterface
+{
+    /**
+     * @var array
+     */
+    private $factories;
+
+    /**
+     * @var FunctionsInterface
+     */
+    private $functions;
+
+
+    /**
+     * @param FunctionsInterface $functions
+     * @param array $list
+     */
+    public function __construct(FunctionsInterface $functions, array $list = array())
+    {
+        $this->factories = array();
+        $this->functions = $functions;
+
+        parent::__construct($list);
+    }
+
+    /**
+     * @param MatchableInterface $factory
+     */
+    public function addFactory(MatchableInterface $factory)
+    {
+        $this->factories[] = $factory;
+    }
+
+    /**
+     * @param string $name
+     * @param array $args
+     * @return mixed
+     */
+    public function make($name, array $args = array())
+    {
+        $class = $this->getClass($name);
+        $factory = $this->match($class);
+        return $factory->makeByClass($class, $args);
+    }
+
+    /**
+     * @param string $class
+     * @return MatchableInterface
+     */
+    protected function match($class)
+    {
+        if (!$interfaces = $this->functions->classImplements($class)) {
+            throw new \UnexpectedValueException("Factory must implement least one interface '{$class}'");
+        }
+
+        foreach ($interfaces as $interface) {
+            foreach ($this->factories as $factory) {
+                if ($factory->match($interface)) {
+                    return $factory;
+                }
+            }
+        }
+
+        throw new \LogicException("No matcher found for '{$class}'");
+    }
+}
---
+++ tests/unit/src/Concretes/RegisterableFactoryInjectableTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Concretehouse\Dp\Factory\Test\Unit\Concretes;
+
+use Concretehouse\Dp\Factory\Concretes\RegisterableFactoryInjectable;
+use Phake;
+
+/**
+ * Test for registerable & factory-injectable factory class.
+ */
+class RegisterableFactoryInjectableTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        // Prepare matcher mocks
+        $this->matchers = array(
+            Phake::mock('Concretehouse\Dp\Factory\MatchableInterface'),
+            Phake::mock('Concretehouse\Dp\Factory\MatchableInterface'),
+            Phake::mock('Concretehouse\Dp\Factory\MatchableInterface'),
+        );
+
+        // prepare matcher mocks return
+        Phake::when($this->matchers[0])->match(false);
+        Phake::when($this->matchers[1])->match(false);
+        Phake::when($this->matchers[2])->match(false);
+
+        // Classes
+        $this->classes = array(
+            'hoge' => 'HogeClass',
+            'fuga' => 'FugaClass',
+            'vaa'  => 'VaaClass',
+        );
+
+        // Prepare functions mock
+        $this->functions = Phake::mock('Concretehouse\Dp\Factory\FunctionsInterface');
+
+        // Prepare registerable factory-injectable factory
+        $this->factory = new RegisterableFactoryInjectable($this->functions, $this->classes);
+
+        // Add factories
+        foreach ($this->matchers as $matcher) {
+            $this->factory->addFactory($matcher);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function implementsMatcherInjectableIF()
+    {
+        $this->assertInstanceOf('Concretehouse\Dp\Factory\MatcherInjectableInterface', $this->factory);
+    }
+
+    /**
+     * @test
+     */
+    public function extendsRegisterable()
+    {
+        $this->assertInstanceOf('Concretehouse\Dp\Factory\Registerable', $this->factory);
+    }
+
+    /**
+     * @test
+     */
+    public function canSetMatchersFromCtor()
+    {
+        $std = new \stdClass;
+
+        Phake::when($this->functions)->classImplements('FugaClass')->thenReturn(array('FugaInterface'));
+
+        Phake::when($this->matchers[1])->match('FugaInterface')->thenReturn(true);
+        Phake::when($this->matchers[1])->makeByClass('FugaClass', array())->thenReturn($std);
+
+        $this->assertSame($std, $this->factory->make('fuga'));
+    }
+
+    /**
+     * @test
+     * @expectedException \UnexpectedValueException
+     */
+    public function throwsExceptionIfMatchedClassHasNoIF()
+    {
+        Phake::when($this->functions)->classImplements('FugaClass')->thenReturn(false);
+        $this->factory->make('fuga');
+    }
+
+    /**
+     * @test
+     * @expectedException \LogicException
+     */
+    public function throwsExceptionIfNoMatcherFound()
+    {
+        Phake::when($this->functions)->classImplements('FugaClass')->thenReturn(array('FugaInterface'));
+        $this->factory->make('fuga');
+    }
+}
