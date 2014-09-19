6ad69b984b9160fbc86c2bc152156c022f75ab3e
tamakiii <tamaki.htm@gmail.com>
2014-09-19 22:15:14 +0900

    add MatchableInterface

---
+++ tests/unit/src/MatchableInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Concretehouse\Dp\Factory\Test\Unit;
+
+use Phake;
+
+/**
+ * Test for matchable factory interface.
+ */
+class MatchableInterfaceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        $this->matchable = Phake::mock('Concretehouse\Dp\Factory\MatchableInterface');
+    }
+
+    /**
+     * @test
+     */
+    public function implementsFactoryInterface()
+    {
+        $this->assertInstanceOf('Concretehouse\Dp\Factory\FactoryInterface', $this->matchable);
+    }
+}
