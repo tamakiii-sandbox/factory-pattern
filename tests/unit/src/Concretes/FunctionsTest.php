9ed922d4d2f5eb292aeb010b201880555006a59c
tamakiii <tamaki.htm@gmail.com>
2014-09-19 22:42:09 +0900

    add tests for Concretes\Functions, and implemented new method newInstanceArgs()

--- tests/unit/src/Concretes/FunctionsTest.php
+++ tests/unit/src/Concretes/FunctionsTest.php
@@ -5,6 +5,12 @@ namespace Concretehouse\Dp\Test\Unit\Concretes;
 use Concretehouse\Dp\Factory\Concretes\Functions;
 use Phake;
 
+interface HInterface {}
+interface HogeInterface {}
+
+class HogeClass implements HInterface, HogeInterface {}
+class FugaClass {}
+
 /**
  * Test for functions-class for factory.
  */
@@ -24,5 +30,57 @@ class FunctionsClassTest extends \PHPUnit_Framework_TestCase
     public function implementsFunctionsIF()
     {
         $this->assertInstanceOf('Concretehouse\Dp\Factory\FunctionsInterface', $this->functions);
+    }
+
+    /**
+     * @test
+     */
+    public function classImplementsReturnsImplementedInterfaces()
+    {
+        $ns = __NAMESPACE__;
+
+        $interfaces = $this->functions->classImplements(__NAMESPACE__ . '\HogeClass');
+        $this->assertSame(array(
+            "{$ns}\HInterface" => "{$ns}\HInterface",
+            "{$ns}\HogeInterface" => "{$ns}\HogeInterface",
+        ), $interfaces);
+    }
+
+    /**
+     * @test
+     */
+    public function classImplementsReturnsFalseIfNoInterface()
+    {
+        $this->assertSame(array(), $this->functions->classImplements(__NAMESPACE__ . '\FugaClass'));
+    }
+
+    /**
+     * @test
+     * @expectedException \PHPUnit_Framework_Error_Warning
+     */
+    public function classImplementsRaisesWarningIfClassNotFound()
+    {
+        $this->functions->classImplements(__NAMESPACE__ . '\VaaClass');
+    }
+
+    /**
+     * @test
+     */
+    public function newInstanceArgsInstantiatesClassWithArgs()
+    {
+        $array = $this->functions->newInstanceArgs('\SplFixedArray', array(10));
+
+        $this->assertInstanceOf('\SplFixedArray', $array);
+        $this->assertSame(10, $array->getSize());
+    }
+
+    /**
+     * @test
+     */
+    public function newInstanceArgsInstantiatesClassWithoutArgs()
+    {
+        $std = $this->functions->newInstanceArgs('\stdClass', array(10));
+
+        $this->assertInstanceOf('\stdClass', $std);
     }
 }
