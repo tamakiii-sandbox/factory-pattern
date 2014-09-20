56e152a78ce614a4f1c02dfafe3381fa9d66c6ad
Daichi Tamaki <tamaki.htm@gmail.com>
2014-09-21 06:00:49 +0900

    add fixed type interface

---
+++ src/FixedTypeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Concretehouse\Dp\Factory;
+
+/**
+ * Fixed type factory interface.
+ */
+interface FixedTypeInterface
+{
+    /**
+     * @return string $class
+     */
+    public function getType();
+}
